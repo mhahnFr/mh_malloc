b99b5441a0c90855aea26d5763a3d2821fa4ec5c
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-07-31 17:23:41 +0200

    Implemented `calloc`s logic

--- src/malloc.c
+++ src/malloc.c
@@ -1,6 +1,7 @@
-#include <errno.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "malloc_internal.h"
 
@@ -24,11 +25,22 @@ void * malloc(size_t size) {
 }
 
 void * calloc(size_t count, size_t size) {
-    (void) count;
-    (void) size;
+    void * chunk;
+    bool   zeroed    = false;
+    size_t totalSize = size * count; // FIXME: Overflow check!
     
-    errno = ENOMEM;
-    return NULL;
+    if (size <= MALLOC_SMALL_SIZE) {
+        chunk = allocateSmallChunk(&pages.smalls);
+    } else if (size <= MALLOC_MIDDLE_SIZE) {
+        chunk = allocateMediumChunk(&pages.mids, totalSize);
+    } else {
+        chunk  = allocateBigChunk(&pages.bigs, totalSize);
+        zeroed = true;
+    }
+    if (!zeroed) {
+        bzero(chunk, totalSize);
+    }
+    return chunk;
 }
 
 void * realloc(void * ptr, size_t newSize) {
