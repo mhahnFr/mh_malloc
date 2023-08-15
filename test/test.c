fbdfd353d005e172e49b8f30e4c8b59e0f82d88a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-15 14:06:29 +0200

    Added test target in the Xcode project

---
+++ test/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../include/malloc.h"
+
+int main(void) {
+    char * str = malloc(512);
+
+    size_t size = snprintf(str, 512, "Dies %s ein Test der %i malloc Bibliothek!", "ein", 42);
+
+    for (size_t i = 0; i < size; ++i) {
+        printf(" %c ", str[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
