0cbaa288aea633f15f3ce5256305a0cb560bc329
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-17 19:46:36 +0200

    Removed another unneeded function

--- src/malloc_internal.c
+++ src/malloc_internal.c
@@ -1,27 +1,4 @@
-#include <errno.h>
-
 #include "pageHeader.h"
-
-void * allocateBigChunk(struct pageHeader ** begin, size_t size) {
-    struct pageHeader * page = page_allocate();
-    
-    if (page == NULL) {
-        errno = ENOMEM;
-        return NULL;
-    }
-    
-    page->size = size;
-    
-    page->previous = NULL;
-    page->next     = *begin;
-    
-    if (*begin != NULL) {
-        (*begin)->previous = page;
-    }
-    *begin = page;
-    
-    return page + sizeof(struct pageHeader);
-}
 
 struct pageHeader * findPageFor(void * pointer, struct pageHeader * pages) {
     struct pageHeader * it;
