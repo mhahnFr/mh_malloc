76c133fc43e7ae5fd50be8860e84d02a06aa9075
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-18 18:15:16 +0200

    Implemented large deallocations

--- src/zone/zone_internal.c
+++ src/zone/zone_internal.c
@@ -13,3 +13,17 @@ struct chunk * zone_allocateLarge(struct zone * self, size_t size) {
     
     return (void *) page + sizeof(struct pageHeader);
 }
+
+bool zone_deallocateLarge(struct zone * self, struct chunk * chunk) {
+    struct pageHeader * page;
+    for (page = self->pages; page != NULL && (void *) page + sizeof(struct pageHeader) != chunk; page = page->next);
+    
+    if (page == NULL) {
+        return false;
+    }
+    
+    page_remove(&self->pages, page);
+    page_deallocate(page);
+    
+    return true;
+}
