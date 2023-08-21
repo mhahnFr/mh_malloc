3e079ff865b7a22b3648b8608e83cfea962569e3
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-21 17:42:57 +0200

    Implemented very basic micro allocations

--- src/zone/zone_internal.c
+++ src/zone/zone_internal.c
@@ -2,10 +2,37 @@
 
 #include "zone_internal.h"
 
+static inline struct chunk * zone_smallFindFreeChunk(struct zone * self) {
+    if (self->freeChunks == NULL) {
+        return NULL;
+    }
+    
+    struct chunk * toReturn = self->freeChunks;
+    
+    self->freeChunks = self->freeChunks->next;
+    if (self->freeChunks != NULL) {
+        self->freeChunks->previous = NULL;
+    }
+    
+    return toReturn;
+}
+
 struct chunk * zone_allocateSmall(struct zone * self) {
-    // TODO: Implement
-    errno = ENOMEM;
-    return NULL;
+    struct chunk * chunk = zone_smallFindFreeChunk(self);
+    
+    if (chunk == NULL) {
+        // Allocate page, return chunk from there
+        struct pageHeader * page = page_allocate();
+        if (page == NULL) {
+            errno = ENOMEM;
+            return NULL;
+        }
+        page_add(&self->pages, page);
+        
+        // TODO: Add all available chunks to the list
+        chunk = (void *) page + sizeof(struct pageHeader);
+    }
+    return chunk;
 }
 
 struct chunk * zone_allocateMedium(struct zone * self, size_t size) {
@@ -30,8 +57,16 @@ struct chunk * zone_allocateLarge(struct zone * self, size_t size) {
 }
 
 bool zone_deallocateSmall(struct zone * self, struct chunk * chunk) {
-    // TODO: Implement
-    return false;
+    chunk->next = self->freeChunks;
+    chunk->previous = NULL;
+    if (self->freeChunks != NULL) {
+        self->freeChunks->previous = chunk;
+    }
+    self->freeChunks = chunk;
+    
+    // TODO: When to unmap pages?
+    
+    return true;
 }
 
 bool zone_deallocateMedium(struct zone * self, struct chunk * chunk) {
