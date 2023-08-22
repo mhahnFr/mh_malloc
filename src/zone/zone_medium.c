8899852608dc04beec51ffd019b43815794aa496
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-22 14:26:15 +0200

    Implemented first part of medium allocations

--- src/zone/zone_medium.c
+++ src/zone/zone_medium.c
@@ -2,10 +2,33 @@
 
 #include "zone_medium.h"
 
+struct chunk * zone_mediumFindFreeChunk(struct zone * self, size_t size) {
+    if (self->freeChunks == NULL) {
+        return NULL; // TODO: Search in the most recent page
+    }
+    
+    struct chunk * tmp = NULL;
+    for (struct chunk * it = self->freeChunks; it != NULL; it = it->next) {
+        if (it->size >= size && (tmp == NULL || it->size < tmp->size)) {
+            tmp = it;
+        }
+    }
+    return tmp; // TODO: If null, check recent page
+}
+
 struct chunk * zone_allocateMedium(struct zone * self, size_t size) {
-    // TODO: Implement
-    errno = ENOMEM;
-    return NULL;
+    struct chunk * chunk = zone_mediumFindFreeChunk(self, size);
+    
+    if (chunk == NULL) {
+        struct pageHeader * page = page_allocate();
+        if (page == NULL) {
+            errno = ENOMEM;
+            return NULL;
+        }
+        chunk = (void *) page + sizeof(struct pageHeader);
+    }
+    chunk->size = size;
+    return chunk;
 }
 
 bool zone_deallocateMedium(struct zone * self, struct chunk * chunk, struct pageHeader * hint) {
--- test/test.c
+++ test/test.c
@@ -3,41 +3,41 @@
 #include "../include/malloc.h"
 
 int main(void) {
-//    char * str = malloc(512);
+    char * str = malloc(512);
+
+    size_t size = snprintf(str, 512, "Dies %s ein Test der %i malloc Bibliothek!", "ein", 42);
+
+    for (size_t i = 0; i < size; ++i) {
+        printf(" %c ", str[i]);
+    }
+    printf("\n");
+    printf("Page size: %zu\n", *(((size_t *) str) - 2));
+    printf("Allocation size: %zu\n", *(((size_t *) str) - 1));
+//    free(str);
+//    free(str);
+    
+//    char * allocs[50000];
 //
-//    size_t size = snprintf(str, 512, "Dies %s ein Test der %i malloc Bibliothek!", "ein", 42);
-//
-//    for (size_t i = 0; i < size; ++i) {
-//        printf(" %c ", str[i]);
+//    for (size_t i = 0; i < 50000; ++i) {
+//        allocs[i] = malloc(0);
 //    }
-//    printf("\n");
-//    printf("Page size: %zu\n", *(((size_t *) str) - 2));
-//    printf("Allocation size: %zu\n", *(((size_t *) str) - 1));
-//    free(str);
-//    free(str);
-    
-    char * allocs[50000];
-    
-    for (size_t i = 0; i < 50000; ++i) {
-        allocs[i] = malloc(0);
-    }
-    
-    for (size_t i = 0; i < 25000; ++i) {
-        free(allocs[i]);
-    }
-    
-    char * allocs2[50000];
-    for (size_t i = 0; i < 50000; ++i) {
-        allocs2[i] = malloc(0);
-    }
-    
-    for (size_t i = 25000; i < 50000; ++i) {
-        free(allocs[i]);
-    }
-    
-    for (size_t i = 0; i < 50000; ++i) {
-        free(allocs2[i]);
-    }
+//
+//    for (size_t i = 0; i < 25000; ++i) {
+//        free(allocs[i]);
+//    }
+//
+//    char * allocs2[50000];
+//    for (size_t i = 0; i < 50000; ++i) {
+//        allocs2[i] = malloc(0);
+//    }
+//
+//    for (size_t i = 25000; i < 50000; ++i) {
+//        free(allocs[i]);
+//    }
+//
+//    for (size_t i = 0; i < 50000; ++i) {
+//        free(allocs2[i]);
+//    }
 
     printf("Hi\n");
     return 0;
