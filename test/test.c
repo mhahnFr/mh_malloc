dc99849d5794f0264fd392e9b1f13923ef85c451
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-28 18:28:42 +0200

    Extended test main

--- test/test.c
+++ test/test.c
@@ -1,8 +1,19 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "../include/malloc.h"
 
 #define ALLOC_SIZE 17
+
+bool value(char * array, char expected, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        if (array[i] != expected) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(void) {
 //    char * str = malloc(512);
@@ -21,10 +32,17 @@ int main(void) {
     char * allocs[5000];
 
     for (size_t i = 0; i < 5000; ++i) {
-        allocs[i] = malloc(ALLOC_SIZE);
+        allocs[i] = malloc(ALLOC_SIZE + i);
     }
 
+    {
+        for (size_t i = 0; i < 5000; ++i) {
+            __builtin_memset(allocs[i], 'z', ALLOC_SIZE + i);
+        }
+    }
+    
     for (size_t i = 0; i < 2500; ++i) {
+        assert(value(allocs[i], 'z', ALLOC_SIZE + i));
         free(allocs[i]);
     }
 
@@ -32,12 +50,19 @@ int main(void) {
     for (size_t i = 0; i < 5000; ++i) {
         allocs2[i] = malloc(ALLOC_SIZE);
     }
+    {
+        for (size_t i = 0; i < 5000; ++i) {
+            __builtin_memset(allocs[i], 'Z', ALLOC_SIZE);
+        }
+    }
 
     for (size_t i = 2500; i < 5000; ++i) {
+        assert(value(allocs[i], 'z', ALLOC_SIZE + i));
         free(allocs[i]);
     }
 
     for (size_t i = 0; i < 5000; ++i) {
+        assert(value(allocs2[i], 'Z', ALLOC_SIZE));
         free(allocs2[i]);
     }
 
