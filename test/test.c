e5d7c3ceeb565a9f556002e393d851b2b63db0e6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-28 18:40:13 +0200

    Testing with more different values

--- test/test.c
+++ test/test.c
@@ -6,6 +6,8 @@
 
 #define ALLOC_SIZE 17
 
+const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
 bool value(char * array, char expected, size_t length) {
     for (size_t i = 0; i < length; ++i) {
         if (array[i] != expected) {
@@ -16,6 +18,7 @@ bool value(char * array, char expected, size_t length) {
 }
 
 int main(void) {
+    const size_t len = __builtin_strlen(alphabet);
 //    char * str = malloc(512);
 //
 //    size_t size = snprintf(str, 512, "Dies %s ein Test der %i malloc Bibliothek!", "ein", 42);
@@ -37,32 +40,32 @@ int main(void) {
 
     {
         for (size_t i = 0; i < 5000; ++i) {
-            __builtin_memset(allocs[i], 'z', ALLOC_SIZE + i);
+            __builtin_memset(allocs[i], alphabet[i % len], ALLOC_SIZE + i);
         }
     }
     
     for (size_t i = 0; i < 2500; ++i) {
-        assert(value(allocs[i], 'z', ALLOC_SIZE + i));
+        assert(value(allocs[i], alphabet[i % len], ALLOC_SIZE + i));
         free(allocs[i]);
     }
 
     char * allocs2[5000];
     for (size_t i = 0; i < 5000; ++i) {
-        allocs2[i] = malloc(ALLOC_SIZE);
+        allocs2[i] = malloc(ALLOC_SIZE * (i % 10 + 1));
     }
     {
         for (size_t i = 0; i < 5000; ++i) {
-            __builtin_memset(allocs2[i], 'Z', ALLOC_SIZE);
+            __builtin_memset(allocs2[i], alphabet[i % len], ALLOC_SIZE * (i % 10 + 1));
         }
     }
 
     for (size_t i = 2500; i < 5000; ++i) {
-        assert(value(allocs[i], 'z', ALLOC_SIZE + i));
+        assert(value(allocs[i], alphabet[i % len], ALLOC_SIZE + i));
         free(allocs[i]);
     }
 
     for (size_t i = 0; i < 5000; ++i) {
-        assert(value(allocs2[i], 'Z', ALLOC_SIZE));
+        assert(value(allocs2[i], alphabet[i % len], ALLOC_SIZE * (i % 10 + 1)));
         free(allocs2[i]);
     }
 
