fc891b083d9c85f50156f6c9e90000561fbc3581
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-29 19:22:39 +0200

    Fixed compilation error, added bit masks

--- src/zone/chunk.h
+++ src/zone/chunk.h
@@ -8,7 +8,7 @@ struct chunk {
     
     void * next;
     void * previous;
-}
+};
 
 static inline struct chunk * chunk_fromPointer(void * pointer) {
     return pointer - sizeof(chunk_flagType);
@@ -17,5 +17,10 @@ static inline struct chunk * chunk_fromPointer(void * pointer) {
 static inline void * chunk_toPointer(struct chunk * self) {
     return (void *) self + sizeof(chunk_flagType);
 }
+
+#define CHUNK_FREED  0b00000001
+#define CHUNK_SMALL  0b00000010
+#define CHUNK_MEDIUM 0b00000100
+#define CHUNK_LARGE  0b00001000
 
 #endif /* chunk_h */
