9f81203f66d6846d3406b6004d5bc8f9b6de8cc7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-30 18:08:21 +0200

    Implemented large chunk structure

---
+++ src/zone/large/chunk_large.h
@@ -0,0 +1,30 @@
+#ifndef chunk_large_h
+#define chunk_large_h
+
+#include "../chunk.h"
+
+struct chunkLarge {
+    size_t         size;
+    chunk_flagType flag;
+    
+    struct chunkLarge * next;
+    struct chunkLarge * previous;
+};
+
+static inline struct chunkLarge * chunkLarge_fromChunk(struct chunk * chunk) {
+    return (void *) chunk - sizeof(size_t);
+}
+
+static inline struct chunkLarge * chunkLarge_fromPointer(void * pointer) {
+    return chunkLarge_fromChunk(chunk_fromPointer(pointer));
+}
+
+static inline struct chunk * chunkLarge_toChunk(struct chunkLarge * self) {
+    return (void *) self + sizeof(size_t);
+}
+
+static inline void * chunkLarge_toPointer(struct chunkLarge * self) {
+    return chunk_toPointer(chunkLarge_toChunk(self));
+}
+
+#endif /* chunk_large_h */
