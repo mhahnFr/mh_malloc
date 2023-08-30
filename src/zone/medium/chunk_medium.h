10177505c887a585b1dde83b1b17e3daae2df36d
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-30 19:03:24 +0200

    Implemented medium chunk

---
+++ src/zone/medium/chunk_medium.h
@@ -0,0 +1,31 @@
+#ifndef chunk_medium_h
+#define chunk_medium_h
+
+#include "../chunk.h"
+
+struct chunkMedium {
+    size_t size;
+    
+    chunk_flagType flag;
+    
+    struct chunkMedium * next;
+    struct chunkMedium * previous;
+};
+
+static inline struct chunkMedium * chunkMedium_fromChunk(struct chunk * chunk) {
+    return (void *) chunk - sizeof(size_t);
+}
+
+static inline struct chunkMedium * chunkMedium_fromPointer(void * pointer) {
+    return chunkMedium_fromChunk(chunk_fromPointer(pointer));
+}
+
+static inline struct chunk * chunkMedium_toChunk(struct chunkMedium * self) {
+    return (void *) self + sizeof(size_t);
+}
+
+static inline void * chunkMedium_toPointer(struct chunkMedium * self) {
+    return chunk_toPointer(chunkMedium_toChunk(self));
+}
+
+#endif /* chunk_medium_h */
