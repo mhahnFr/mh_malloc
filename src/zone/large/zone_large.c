d9b6860ab09eaf9151cf7fa43d44c8300cf1ccc4
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-06 19:40:34 +0200

    Implemented first half of large reallocations

--- src/zone/large/zone_large.c
+++ src/zone/large/zone_large.c
@@ -17,6 +17,8 @@ void * zone_allocateLarge(struct zone * self, size_t size) {
     chunk->flag   = 0;
     chunk->flag  |= CHUNK_LARGE;
     
+    page->pageLocal.counted.allocCount = size;
+    
     return chunk_toPointer(chunk);
 }
 
@@ -28,4 +30,14 @@ bool zone_deallocateLarge(struct zone * self, void * pointer) {
     page_deallocate(page);
     
     return true;
+}
+
+bool zoneLarge_enlarge(struct zone * self, void * pointer, size_t newSize) {
+    // TODO: Implement
+    
+    return false;
+}
+
+size_t zoneLarge_getAllocationSize(void * pointer) {
+    return ((struct pageHeader *) (void *) chunk_fromPointer(pointer) - sizeof(struct pageHeader))->pageLocal.counted.allocCount;
 }
--- src/zone/large/zone_large.h
+++ src/zone/large/zone_large.h
@@ -6,4 +6,7 @@
 void * zone_allocateLarge(struct zone * self, size_t bytes);
 bool zone_deallocateLarge(struct zone * self, void * pointer);
 
+size_t zoneLarge_getAllocationSize(void * pointer);
+bool zoneLarge_enlarge(struct zone * self, void * pointer, size_t newSize);
+
 #endif /* zone_large_h */
