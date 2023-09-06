fb44afed9f8a8aa4bb0ce53df89de731d1e9fc62
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-06 17:07:01 +0200

    Corrected regular chunk overhead

--- src/zone/chunk.h
+++ src/zone/chunk.h
@@ -10,14 +10,16 @@ struct chunk {
     void * previous;
 };
 
+#define CHUNK_OVERHEAD (sizeof(struct chunk) - 2 * sizeof(void *))
+
 #define CHUNK_MINIMUM_SIZE (sizeof(void *) * 2)
 
 static inline struct chunk * chunk_fromPointer(void * pointer) {
-    return pointer - sizeof(chunk_flagType);
+    return pointer - CHUNK_OVERHEAD;
 }
 
 static inline void * chunk_toPointer(struct chunk * self) {
-    return (void *) self + sizeof(chunk_flagType);
+    return (void *) self + CHUNK_OVERHEAD;
 }
 
 #define CHUNK_FREED  0b00000001
