6edda041413bb34355f153f24148506b944e16c2
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-12 20:13:12 +0200

    Documented `malloc_error`

--- src/error.h
+++ src/error.h
@@ -1,6 +1,11 @@
 #ifndef error_h
 #define error_h
 
+/**
+ * Prints the given error message and aborts the program.
+ *
+ * @param message the message to be printed
+ */
 void malloc_error(const char * message);
 
 #endif /* error_h */
