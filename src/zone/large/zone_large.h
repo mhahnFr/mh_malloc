39a75f06abe00bf5b8fd85be127e5ea7bb89ffd0
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-12 20:11:38 +0200

    Documented the public functions of the large zone

--- src/zone/large/zone_large.h
+++ src/zone/large/zone_large.h
@@ -3,10 +3,37 @@
 
 #include "../zone.h"
 
+/**
+ * Allocates the given amount of bytes in the given large zone.
+ *
+ * @param self the large zone object
+ * @param bytes the amount of bytes to be allocated
+ * @return the user pointer or `NULL` if unable to allocate
+ */
 void * zoneLarge_allocate(struct zone * self, size_t bytes);
+/**
+ * Deallocates the given user pointer in the given large zone.
+ *
+ * @param self the large zone object
+ * @param pointer the user pointer to be deallocated
+ * @return whether the pointer was deallocated successfully
+ */
 bool zoneLarge_deallocate(struct zone * self, void * pointer);
 
+/**
+ * Returns the size of the memory block pointed to by the given pointer.
+ *
+ * @param pointer the user pointer
+ * @return the size of the associated memory block
+ */
 size_t zoneLarge_getAllocationSize(void * pointer);
+/**
+ * Tries to enlarge the memory block pointed to by the given user pointer.
+ *
+ * @param pointer the user pointer
+ * @param newSize the new size
+ * @return whether the memory block could be enlarged
+ */
 bool zoneLarge_enlarge(void * pointer, size_t newSize);
 
 #endif /* zone_large_h */
