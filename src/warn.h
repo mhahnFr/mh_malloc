623a0727b6ea0eea5353aa3cabaf1d8ae5f749b5
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-12 20:13:54 +0200

    Documented `malloc_warn`

--- src/warn.h
+++ src/warn.h
@@ -1,6 +1,11 @@
 #ifndef warn_h
 #define warn_h
 
+/**
+ * Prints the given warning message.
+ *
+ * @param message the message to be printed
+ */
 void malloc_warn(const char * message);
 
 #endif /* warn_h */
