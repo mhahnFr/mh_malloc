e7d7c00bf2293c0578bb112c9c4d4a6fb4c84bc6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-14 23:49:50 +0200

    Documented the zones

--- src/zone/zones.h
+++ src/zone/zones.h
@@ -10,14 +10,28 @@
 
 #include "../pageHeader.h"
 
+/**
+ * This structure represents a collection of allocation zones.
+ */
 struct zones {
+    /** Whether the zones have been initialized. */
     bool inited;
     
+    /** The small allocations zone.              */
     struct zone small;
+    /** The medium allocations zone.             */
     struct zone medium;
+    /** The large allocations zone.              */
     struct zone large;
 };
 
+/**
+ * Returns the appropriate zone for allocation the given amount of bytes.
+ *
+ * @param self the zones collection
+ * @param size the amount of bytes to be allocated
+ * @return the appropriate zone
+ */
 static inline struct zone * zones_getZoneBySize(struct zones * self, size_t size) {
     if (!self->inited) {
         const size_t pageSize = page_getPageSize() * PAGE_FACTOR;
@@ -35,6 +49,12 @@ static inline struct zone * zones_getZoneBySize(struct zones * self, size_t size
     return &self->large;
 }
 
+/**
+ * Returns the zone the given pointer comes from.
+ *
+ * @param pointer the pointer whose zone to return
+ * @return the zone of the pointer or `NULL`, if the given pointer does not belong to a zone
+ */
 static inline struct zone * zones_getZoneByPointer(void * pointer) {
     struct chunk * chunk = chunk_fromPointer(pointer);
     
