ad275596bf41e1c7202f7745c24c69d0ea6eb4cb
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-19 19:48:49 +0200

    Documented the main header

--- include/malloc.h
+++ include/malloc.h
@@ -3,9 +3,40 @@
 
 #include <stddef.h>
 
-void * malloc(size_t);
-void * calloc(size_t, size_t);
-void * realloc(void *, size_t);
-void   free(void *);
+/**
+ * Allocates the given amount of memory.
+ *
+ * @param size the amount of bytes to be allocated
+ * @return a pointer to the memory block or `NULL` if no memory is available
+ */
+void * malloc(size_t size);
+/**
+ * Allocates the given amount of zeroed bytes.
+ *
+ * @param count the amount of objects
+ * @param size the size of an object
+ * @return a pointer to the memory block or `NULL` if no memory is available
+ */
+void * calloc(size_t size, size_t count);
+/**
+ * @brief Attempts to resize the given allocation to the new size.
+ *
+ * If this is not possible, a new allocation is created and the content is copied.
+ * The original allocation is then deallocated. If no new allocation could be
+ * created, `NULL` is returned and the given allocation remains valid.
+ *
+ * @param pointer the old allocation
+ * @param size the new size
+ * @return a pointer to the reallocated memory
+ */
+void * realloc(void * pointer, size_t size);
+/**
+ * @brief Deallocates the given allocation.
+ *
+ * Does nothing if `NULL` is passed.
+ *
+ * @param pointer the allocation to be deallocated
+ */
+void   free(void * pointer);
 
 #endif /* malloc_h */
