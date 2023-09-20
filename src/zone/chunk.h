46bda556c76c06467c7cac40e351d420c06c1327
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-20 16:09:33 +0200

    Added license notice to all files

--- src/zone/chunk.h
+++ src/zone/chunk.h
@@ -1,3 +1,22 @@
+/*
+ * mh_malloc - Small memory management library.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef chunk_h
 #define chunk_h
 
